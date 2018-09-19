2332654b9afdf172c9c83e8df1f2700916ef089c
Ryan Brady <ryanbrady369@gmail.com>
2018-09-19 19:40:05 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,42 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  end
+  true
+end
+
+def valid_move?(board,index)
+  index = index.to_i
+  if !position_taken?(board, index) && index < 10 && index > 0
+    return true
+  end
+  false
+end
+
+
+def move(board,input,letter = "x")
+  input = input.to_i - 1
+  board[input] = letter
+end
+
+
+def turn(board)
+  puts "Please enter 1-9:"
+  desired_move = gets.strip
+  if valid_move?(board, desired_move)
+    move(board,desired_move)
+#    display_board(board)
+  else
+    puts "This position is either taken or invalid"
+    turn(board)
+  end
+end
