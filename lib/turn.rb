8c3cd503bd040a6bcfea3ad1e1552ae8c5ffa9e1
Ryan Brady <ryanbrady369@gmail.com>
2018-09-19 19:40:16 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,7 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+turn_count = 1
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -5,6 +9,15 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def board_reset(board)
+  curr = 0
+  while curr < 9
+    board[curr] = " "
+    curr += 1
+  end
+end
+
 
 
 def position_taken?(board, index)
@@ -15,28 +28,84 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board,index)
-  index = index.to_i
-  if !position_taken?(board, index) && index < 10 && index > 0
+  if !position_taken?(board, index) && index < 10 && index >= 0
     return true
   end
   false
 end
 
-
-def move(board,input,letter = "x")
+def input_to_index(input)
   input = input.to_i - 1
-  board[input] = letter
 end
 
 
-def turn(board)
+
+def move(board,index,turn_count)
+  turn_count % 2 == 0 ? player_turn = "o" : player_turn = "x"
+  board[index] = player_turn
+end
+
+
+
+
+def game_over?(board)
+  curr = 0
+  while curr <= 8
+    if board[curr] != " " && (board[curr] == board[curr + 1] && board[curr] == board[curr + 2])
+      display_board(board)
+      puts "#{board[curr]} is the winner!"
+      return true
+    elsif  board[curr] != " " && (board[curr] == board[curr + 3] && board[curr] == board[curr + 6])
+      display_board(board)
+      puts "#{board[curr]} is the winner!"
+      return true
+    else
+      curr += 3
+    end
+  end
+  if  board[curr] != " " && ((board[0] == board[4] && board[8] == board[4]) || (board[2] == board[4] && board[6] == board[4]))
+    display_board(board)
+    puts "#{board[4]} is the winnner!"
+    return true
+  end
+  return false
+end
+
+
+
+
+def turn(board, turn_count)
+
+  display_board(board)
+
+  if turn_count > 5
+    game_over?(board)
+    if game_over?(board)
+      puts "play again?"
+      reset = gets.strip
+      if reset == "y" || reset == "yes"
+        board_reset(board)
+        turn_count = 1
+        turn(board, turn_count)
+      end
+    end
+  end
+
   puts "Please enter 1-9:"
+
   desired_move = gets.strip
-  if valid_move?(board, desired_move)
-    move(board,desired_move)
-#    display_board(board)
+
+  desired_move = input_to_index(desired_move)
+
+  if valid_move?(board,desired_move)
+    move(board,desired_move,turn_count)
+    turn_count += 1
+    turn(board, turn_count)
   else
     puts "This position is either taken or invalid"
-    turn(board)
+    turn(board, turn_count)
   end
 end
+
+
+#turn(board, turn_count)
